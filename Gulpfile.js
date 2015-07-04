447c1377c8ceaa26a33b56943f0aadf475005718
Joe Haines <hello@joehaines.co.uk>
2015-07-05 00:15:11 +0100

    Use min reporter for watching, otherwise use spec

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp')
 var mocha = require('gulp-mocha')
 var notify = require('gulp-notify')
 
+var watch = false
+
 gulp.task('test', function () {
+  var reporter = watch ? 'min' : 'spec'
+
   return gulp.src('spec/spec.js', {read: false})
-    .pipe(mocha({reporter: 'spec'}))
+    .pipe(mocha({reporter: reporter }))
     .on('error', notify.onError({
       title: 'Oops!',
       message: 'Something broke'
@@ -12,6 +16,7 @@ gulp.task('test', function () {
 })
 
 gulp.task('watch', function () {
+  watch = true
   gulp.watch(['**/*.js', '!./node_modules/**'], ['test'])
 })
 
