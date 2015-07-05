cf6305213916792aa3e9f672f7e67a86a88a9593
Joe Haines <hello@joehaines.co.uk>
2015-07-05 09:38:39 +0100

    Add output and list functions

--- nickd.js
+++ nickd.js
@@ -1,17 +1,28 @@
 var extend = require('lodash/object/extend')
 var without = require('lodash/array/without')
 
-var Nickd = function () {
-  this.list = []
+var Nickd = function (cli) {
+  this.tasks = []
+  this.cli = cli ? cli : require('clicolor').cli()
 }
 
 Nickd.prototype = extend(Nickd.prototype, {
   add: function add (task) {
-    this.list.push(task)
+    this.tasks.push(task)
   },
 
   remove: function remove (task) {
-    this.list = without(this.list, task)
+    this.tasks = without(this.tasks, task)
+  },
+
+  list: function list () {
+    this.tasks.map(function (task) {
+      this.output(task)
+    }.bind(this))
+  },
+
+  output: function output (message) {
+    this.cli.display(message)
   }
 })
 
--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,15 @@
-var describe = require('mocha').describe
-var it = require('mocha').it
+var mocha = require('mocha')
 var expect = require('expect')
+var sinon = require('sinon')
+var clicolor = require('clicolor')
+var cli = clicolor.cli()
+
 var Nickd = require('../nickd')
+
+var describe = mocha.describe
+var it = mocha.it
+var beforeEach = mocha.beforeEach
+var afterEach = mocha.afterEach
 
 describe('nickd', function () {
   it('should be initialisable', function () {
@@ -10,8 +18,8 @@ describe('nickd', function () {
     }).toNotThrow()
   })
 
-  it('should initialise list as an empty array', function () {
-    expect(new Nickd().list).toEqual([])
+  it('should initialise the task list as an empty array', function () {
+    expect(new Nickd().tasks).toEqual([])
   })
 
   describe('add', function () {
@@ -23,14 +31,14 @@ describe('nickd', function () {
     it('should add a task to the list', function () {
       var nickd = new Nickd()
       nickd.add('a task')
-      expect(nickd.list).toEqual(['a task'])
+      expect(nickd.tasks).toEqual(['a task'])
     })
 
     it('should add multiple tasks to the list', function () {
       var nickd = new Nickd()
       nickd.add('a task')
       nickd.add('another task')
-      expect(nickd.list).toEqual(['a task', 'another task'])
+      expect(nickd.tasks).toEqual(['a task', 'another task'])
     })
   })
 
@@ -44,7 +52,7 @@ describe('nickd', function () {
       var nickd = new Nickd()
       nickd.add('a task')
       nickd.remove('a task')
-      expect(nickd.list).toEqual([])
+      expect(nickd.tasks).toEqual([])
     })
 
     it('should only remove one task', function () {
@@ -52,7 +60,7 @@ describe('nickd', function () {
       nickd.add('a task')
       nickd.add('another task')
       nickd.remove('a task')
-      expect(nickd.list).toEqual(['another task'])
+      expect(nickd.tasks).toEqual(['another task'])
     })
 
     it('should do nothing when the given task isn\'t in the list', function () {
@@ -60,7 +68,58 @@ describe('nickd', function () {
       nickd.add('a task')
       nickd.add('another task')
       nickd.remove('a non-existant task')
-      expect(nickd.list).toEqual(['a task', 'another task'])
+      expect(nickd.tasks).toEqual(['a task', 'another task'])
+    })
+  })
+
+  describe('output', function () {
+    it('should exist', function () {
+      expect(new Nickd().output).toExist()
+    })
+
+    it('should log whatever is passed to it', function () {
+      var nickd = new Nickd(cli)
+      var stub = sinon.stub(cli, 'display')
+      nickd.output('test')
+
+      expect(stub.calledWith('test')).toBe(true)
+      cli.display.restore()
+    })
+  })
+
+  describe('list', function () {
+    var stub
+    beforeEach(function () {
+      stub = sinon.stub(cli, 'display')
+    })
+
+    afterEach(function () {
+      cli.display.restore()
+    })
+
+    it('should exist', function () {
+      var nickd = new Nickd()
+      expect(nickd.list).toExist()
+    })
+
+    it('should log a task in the list', function () {
+      var nickd = new Nickd(cli)
+      nickd.add('a task')
+      nickd.list()
+
+      expect(stub.calledWith('a task')).toBe(true)
+    })
+
+    it('should log all tasks in the list', function () {
+      var nickd = new Nickd(cli)
+      nickd.add('a task')
+      nickd.add('another task')
+      nickd.add('a third task')
+      nickd.list()
+
+      expect(stub.calledWith('a task')).toBe(true)
+      expect(stub.calledWith('another task')).toBe(true)
+      expect(stub.calledWith('a third task')).toBe(true)
     })
   })
 })
